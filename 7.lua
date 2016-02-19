30521f2930f425f2b81eb1b777c060477eaaa8ed
rreed <rpreed@protonmail.com>
2016-02-18 16:34:05 -0800

    Problem 7, Lua

---
+++ 7.lua
@@ -0,0 +1,25 @@
+--[[
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+Usage: lua 7.lua
+]]
+
+function nthPrime(n)
+	local r = 1
+	for i = 1, n do
+		while true do
+			r = r + 1
+			local isPrime = true
+			for j = 2, r-1 do
+				isPrime = r % j ~= 0
+				if not isPrime then break end
+			end
+                        if isPrime then break end
+		end
+	end
+	return r
+end
+
+print(nthPrime(10001))
