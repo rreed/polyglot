4e8eaa6fd44a8f77396459afa67cddfe129d99c5
rreed <rpreed@protonmail.com>
2016-03-07 14:28:57 -0800

    Problem 16, Scala

---
+++ 16/Sixteen.scala
@@ -0,0 +1,15 @@
+/*
+ 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26. 
+ What is the sum of the digits of the number 2^1000?
+*/
+
+object Sixteen {
+  def main(args: Array[String]) {
+    val bigString = BigInt(2).pow(1000).toString
+    var ret = 0
+    for (c <- bigString) {
+      ret += c.asDigit // toInt returns the ASCII value of this char. Lesson learned. :)
+    }
+    println(ret)
+  }
+}
